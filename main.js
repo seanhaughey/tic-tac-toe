defc21368a3aa211406868d04275d08d82c773e9
Sean Haughey <sdhaughey@gmail.com>
2015-09-09 15:07:22 -0500

    fixed square buttons positioning bug

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(function() {
 	// confirm("Shall we play a game?");
 	// confirm("How about tic-tac-toe?");
-	// var playerOne = prompt("Player 1 - Do you choose X's or O's?");
-	
+	var playerOne = prompt("Player 1 - Do you choose X's or O's?");
+	if ((playerOne !== "x") && (playerOne !== "X")) {
+		playerOne = prompt("Choose X or O!")
+	}
+	else if ((playerOne = "x") || (playerOne = "X")) {
+		$("#s1").on("click", function() {
+			$("#s1").html("X");
+		});
+	}
 })
