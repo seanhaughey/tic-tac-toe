0fcb7e64f707c1fb1359d25bcc6684e3e2c6a114
Sean Haughey <sdhaughey@gmail.com>
2015-09-09 15:43:02 -0500

    added player 1 game piece prompt, player2 assignment and on.click function

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ $(function() {
 	// confirm("Shall we play a game?");
 	// confirm("How about tic-tac-toe?");
 	var playerOne = prompt("Player 1 - Do you choose X's or O's?");
-	if ((playerOne !== "x") && (playerOne !== "X")) {
-		playerOne = prompt("Choose X or O!")
+	if ((playerOne == "x") || (playerOne == "X")) {
+		playerTwo = "o";
 	}
-	else if ((playerOne = "x") || (playerOne = "X")) {
-		$("#s1").on("click", function() {
-			$("#s1").html("X");
-		});
-	}
+	else {
+		playerTwo = "x";
+	};
+	$("button").on("click", function(){
+		($(this).html("X"));
+	})
+	// if ((playerOne !== "x") && (playerOne !== "X")) {
+	// 	playerOne = prompt("Choose X or O!")
+	// }
+	// if ((playerOne = "x") || (playerOne = "X")) {
+	// 	$("#s1").on("click", function() {
+	// 		$("#s1").html("X");
+	// 	});
+	// }
 })
