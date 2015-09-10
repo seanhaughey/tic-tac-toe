120ab5c01b4782d1944301e9b0adcedf7e34e157
Sean Haughey <sdhaughey@gmail.com>
2015-09-10 12:35:21 -0500

    set functionality to choose winner or tie

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(function() {
 	var playerState = true;
+	var board = {
+		s1 : "", s2 : "", s3 : "", 
+		s4 : "", s5 : "", s6 : "", 
+		s7 : "", s8 : "", s9 : "",
+	};
+	var movesCounter = 0;
 	// confirm("Shall we play a game?");
 	// confirm("How about tic-tac-toe?");
 	playerOne = prompt("Player 1 - Do you choose X's or O's?");
@@ -22,34 +28,97 @@ $(function() {
 		
 		
 		$("button.square").on("click", function(){
-			if (($("button.square") === "X") || ($("button.square") === "O")) 
+			var boardPlace;
+			var boardPlaceId = $(this).attr('id');
+			boardPlace = $("#" + boardPlaceId);
+
+			if ((boardPlace.html === "X") || (boardPlace.html === "O")) 
 			{
 			return;
 			}
-			
-			else if (playerState == true) {
-				($(this).html("X"));
+			if (playerState == true) {
+				(boardPlace.html("X"));
 				playerState = false;
+				board[boardPlaceId] = "X";
 				}
 			else {
-				($(this).html("O"));
-				playerState = true;	
+				(boardPlace.html("O"));
+				playerState = true;
+				board[boardPlaceId] = "O";
+			}
+			movesCounter ++;
+			if (((board.s1 === "X") && (board.s2 === "X") && (board.s3 === "X")) ||
+				((board.s4 === "X") && (board.s5 === "X") && (board.s6 === "X")) ||
+				((board.s7 === "X") && (board.s8 === "X") && (board.s9 === "X")) ||
+				((board.s1 === "X") && (board.s4 === "X") && (board.s7 === "X")) ||
+				((board.s2 === "X") && (board.s5 === "X") && (board.s8 === "X")) ||
+				((board.s3 === "X") && (board.s6 === "X") && (board.s9 === "X")) ||
+				((board.s1 === "X") && (board.s5 === "X") && (board.s9 === "X")) ||
+				((board.s3 === "X") && (board.s5 === "X") && (board.s7 === "X"))) {
+				alert("X wins!!");
+			}
+			if (((board.s1 === "O") && (board.s2 === "O") && (board.s3 === "O")) ||
+				((board.s4 === "O") && (board.s5 === "O") && (board.s6 === "O")) ||
+				((board.s7 === "O") && (board.s8 === "O") && (board.s9 === "O")) ||
+				((board.s1 === "O") && (board.s4 === "O") && (board.s7 === "O")) ||
+				((board.s2 === "O") && (board.s5 === "O") && (board.s8 === "O")) ||
+				((board.s3 === "O") && (board.s6 === "O") && (board.s9 === "O")) ||
+				((board.s1 === "O") && (board.s5 === "O") && (board.s9 === "O")) ||
+				((board.s3 === "O") && (board.s5 === "O") && (board.s7 === "O"))
+				) {
+				alert("O wins!!")
+			}
+			if (movesCounter === 9){
+				alert("It's a tie!!");
 			}
 		})	
-		}
+	}
 	else {
 		$("button.square").on("click", function(){
-			if (($("button.square") === "X") || ($("button.square") === "O")) {
+			var boardPlace;
+			var boardPlaceId = $(this).attr('id');
+			boardPlace = $("#" + boardPlaceId);
+			if ((boardPlace.html === "X") || (boardPlace.html === "O")) {
 			return;
 			}
-			else if (playerState == true) {
-				($(this).html("O"));
+			if (playerState == true) {
+				(boardPlace.html("O"));
 				playerState = false;
+				board[boardPlaceId] = "O";
 				}
 			else {
-				($(this).html("X"));
-				playerState = true;	
+				(boardPlace.html("X"));
+				playerState = true;
+				board[boardPlaceId] = "X";
 			}
-		})	
-		}
-	})
+			movesCounter ++;
+			if (((board.s1 === "X") && (board.s2 === "X") && (board.s3 === "X")) ||
+				((board.s4 === "X") && (board.s5 === "X") && (board.s6 === "X")) ||
+				((board.s7 === "X") && (board.s8 === "X") && (board.s9 === "X")) ||
+				((board.s1 === "X") && (board.s4 === "X") && (board.s7 === "X")) ||
+				((board.s2 === "X") && (board.s5 === "X") && (board.s8 === "X")) ||
+				((board.s3 === "X") && (board.s6 === "X") && (board.s9 === "X")) ||
+				((board.s1 === "X") && (board.s5 === "X") && (board.s9 === "X")) ||
+				((board.s3 === "X") && (board.s5 === "X") && (board.s7 === "X"))
+			) {
+				alert("X wins!!");
+			}
+			else if (((board.s1 === "O") && (board.s2 === "O") && (board.s3 === "O")) ||
+				((board.s4 === "O") && (board.s5 === "O") && (board.s6 === "O")) ||
+				((board.s7 === "O") && (board.s8 === "O") && (board.s9 === "O")) ||
+				((board.s1 === "O") && (board.s4 === "O") && (board.s7 === "O")) ||
+				((board.s2 === "O") && (board.s5 === "O") && (board.s8 === "O")) ||
+				((board.s3 === "O") && (board.s6 === "O") && (board.s9 === "O")) ||
+				((board.s1 === "O") && (board.s5 === "O") && (board.s9 === "O")) ||
+				((board.s3 === "O") && (board.s5 === "O") && (board.s7 === "O"))
+				) {
+				alert("O wins!!");
+			}
+			else if (movesCounter > 9){
+				alert("It's a tie!!");
+			}	
+		})
+	}
+})
+	
+			
