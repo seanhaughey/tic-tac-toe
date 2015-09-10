420892d6c691941c169f4aa7e7d350e77d8b77b3
Sean Haughey <sdhaughey@gmail.com>
2015-09-10 16:00:50 -0500

    clear board function

--- main.js
+++ main.js
@@ -9,25 +9,20 @@ $(function() {
 	// confirm("Shall we play a game?");
 	// confirm("How about tic-tac-toe?");
 	playerOne = prompt("Player 1 - Do you choose X's or O's?");
+	while ((playerOne !== "x") && (playerOne !== "X") && (playerOne !== "o") && (playerOne !== "O")) {
+		playerOne = prompt ("You must choose X or O!");
+	}
 	if ((playerOne === "x") || (playerOne === "X")) {
 		playerTwo = "o";
 	}
 	else {
 		playerTwo = "x";
 	};
-	// if ((playerOne = "x") || (playerOne = "X")) {
-	// 	playerOneState = "X";
-	// 	playerTwoState = "O";
-	// }
-	// else {
-	// 	playerOneState = "O";
-	// 	playerTwoState = "X";
-	// }
-	
 	if (playerOne === "x") {
 		
 		
 		$("button.square").on("click", function(){
+			var gotWinner;
 			var boardPlace;
 			var boardPlaceId = $(this).attr('id');
 			boardPlace = $("#" + boardPlaceId);
@@ -36,12 +31,12 @@ $(function() {
 			{
 			return;
 			}
-			if (playerState == true) {
+			else if (playerState == true) {
 				(boardPlace.html("X"));
 				playerState = false;
 				board[boardPlaceId] = "X";
 				}
-			else {
+			else if (playerState == false){
 				(boardPlace.html("O"));
 				playerState = true;
 				board[boardPlaceId] = "O";
@@ -55,9 +50,11 @@ $(function() {
 				((board.s3 === "X") && (board.s6 === "X") && (board.s9 === "X")) ||
 				((board.s1 === "X") && (board.s5 === "X") && (board.s9 === "X")) ||
 				((board.s3 === "X") && (board.s5 === "X") && (board.s7 === "X"))) {
+				gotWinner = "yes";
 				alert("X wins!!");
 			}
-			if (((board.s1 === "O") && (board.s2 === "O") && (board.s3 === "O")) ||
+				
+			else if (((board.s1 === "O") && (board.s2 === "O") && (board.s3 === "O")) ||
 				((board.s4 === "O") && (board.s5 === "O") && (board.s6 === "O")) ||
 				((board.s7 === "O") && (board.s8 === "O") && (board.s9 === "O")) ||
 				((board.s1 === "O") && (board.s4 === "O") && (board.s7 === "O")) ||
@@ -66,12 +63,21 @@ $(function() {
 				((board.s1 === "O") && (board.s5 === "O") && (board.s9 === "O")) ||
 				((board.s3 === "O") && (board.s5 === "O") && (board.s7 === "O"))
 				) {
-				alert("O wins!!")
+				gotWinner = "yes";
+				alert("O wins!!");
 			}
-			if (movesCounter === 9){
+			else if (movesCounter === 9){
 				alert("It's a tie!!");
+				gotWinner = "yes";
 			}
-		})	
+			if (gotWinner === "yes") {
+					for (var i = 1; i <= 9; i++) {
+						$('#s' + i).html("Choose this square")
+					};
+				movesCounter = 0;
+				return;
+			}
+		})
 	}
 	else {
 		$("button.square").on("click", function(){
@@ -102,6 +108,7 @@ $(function() {
 				((board.s3 === "X") && (board.s5 === "X") && (board.s7 === "X"))
 			) {
 				alert("X wins!!");
+				gotWinner = "yes";
 			}
 			else if (((board.s1 === "O") && (board.s2 === "O") && (board.s3 === "O")) ||
 				((board.s4 === "O") && (board.s5 === "O") && (board.s6 === "O")) ||
@@ -113,12 +120,22 @@ $(function() {
 				((board.s3 === "O") && (board.s5 === "O") && (board.s7 === "O"))
 				) {
 				alert("O wins!!");
+				gotWinner = "yes";
 			}
 			else if (movesCounter > 9){
 				alert("It's a tie!!");
+				gotWinner = "yes";
+			}
+			if (gotWinner === "yes") {
+					for (var i = 1; i <= 9; i++) {
+						$('#s' + i).html("Choose this square")
+					};
+				movesCounter = 0;
+				return;
 			}	
 		})
 	}
+	return;
 })
 	
 			
